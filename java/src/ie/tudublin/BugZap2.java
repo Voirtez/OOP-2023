ba2c9a7b4a739d7971a59beed2803dec04beeaa6
Voirtez <norbixkrupax@gmail.com>
2023-02-02 09:30:59 +0000

    BugZap2

---
+++ java/src/ie/tudublin/BugZap2.java
@@ -0,0 +1,86 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class BugZap2 extends PApplet
+{
+    float playerX;
+    float playerY;
+    float playerWidth;
+    float halfPlayer;
+
+    float bugX;
+    float bugY;
+    float bugWidth;
+    float halfBug;
+
+	public void settings()
+	{
+		size(500, 500);
+
+	}
+
+	public void setup()
+	{
+        colorMode(HSB);
+        background(0);
+        smooth();
+
+
+        playerX = width * 0.5f;
+        playerY = height - 100;
+
+        playerWidth = 50;
+        halfPlayer = playerWidth * 0.5f;
+
+        resetBug();
+
+	}
+
+    void resetBug()
+    {
+        bugY = 50;
+        bugWidth = 50;
+        halfBug = bugWidth * 0.5f;
+        bugX = random(halfBug, width - halfBug);
+
+    }
+
+    void drawPlayer(float x, float y, float w)
+    {
+        noFill();
+        stroke(255);
+        rectMode(CENTER);
+        rect(x, y, w, w);
+        line(x, y - halfPlayer, x, y - halfPlayer * 2);
+
+    }
+    
+
+	public void keyPressed()
+	{
+		if (keyCode == LEFT)
+		{
+			playerX--;
+			
+		}
+		if (keyCode == RIGHT)
+		{
+			playerX++;
+			
+		}
+		if (key == ' ')
+		{
+			line(playerX, playerY - halfPlayer, playerX, 0);
+		
+		}
+	}
+
+
+	public void draw()
+	{	
+        background(0);
+        drawPlayer(playerX, playerY, playerWidth);
+
+	}	
+}
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,15 +2,15 @@ package ie.tudublin;
 
 public class Main
 {
-	public static void bugZap()
+	public static void bugZap2()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
+        processing.core.PApplet.runSketch( a, new BugZap2());
     }
 	
 	public static void main(String[] args)
 	{
-		bugZap();
+		bugZap2();
 		
 	}
 	
