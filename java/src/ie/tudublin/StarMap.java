1a78e178dbd14903aebf49b3c2a0e8ea3378abaa
Voirtez <norbixkrupax@gmail.com>
2023-02-13 12:06:04 +0000

    w4Class

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -7,9 +7,11 @@ public class StarMap extends PApplet
 	public void settings()
 	{
 		size(500, 500);
+
 	}
 
-	public void setup() {
+	public void setup()
+	{
 		colorMode(HSB);
 		background(0);
 
@@ -17,9 +19,32 @@ public class StarMap extends PApplet
 		
 	}
 
+
+	public void drawGrid()
+	{
+		stroke(100, 255, 255);
+
+		float border = 50.0f;
+		int count = 10;
+		float gap = (width - (border * 2.0f)) / (float) count;
+
+		for(int i = -5; i <= 5; i++)
+		{
+			float x = border + (gap * (i + 5));
+			line(x, border, x, height - border);
+			line(border, x, width - border, x);
+
+		}
+
+		
+
+	}
 		
 	public void draw()
 	{	
-		strokeWeight(2);		
+		strokeWeight(2);
+
+		drawGrid();
+				
 	}
 }
