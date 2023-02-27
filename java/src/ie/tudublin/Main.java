4fce146039e3ad9dff7d71325ae7ea0b066edeb3
Voirtez <norbixkrupax@gmail.com>
2023-02-27 12:56:01 +0000

    audio2

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,46 +2,17 @@ package ie.tudublin;
 
 public class Main
 {
-
-	public static void helloProcessing()
+	public static void audio2()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
-    }
-
-	public static void loops()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
-
-	public static void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
-    }
-
-	public static void starMap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new StarMap());
-    }
-
-	public static void audio1()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Audio1());
-    }
+        processing.core.PApplet.runSketch( a, new Audio2());
+    }	
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world");
 		
-		Dog penny = new Dog();
-		penny.setName("Penny");
-		penny.speak();
 
-		audio1();
+		audio2();
 	}
 	
 }
