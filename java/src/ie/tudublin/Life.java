96f6a1719ee30d6cdaad57934fa20c7688870eb9
Voirtez <norbixkrupax@gmail.com>
2023-03-06 12:55:32 +0000

    gameOfLife

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -5,23 +5,28 @@ import processing.core.PApplet;
 public class Life extends PApplet
 {
 
+	LifeBoard board;
+
 	public void settings()
 	{
 		size(500, 500);
+
 	}
 
-	public void setup() {
-		colorMode(HSB);
+	public void setup()
+	{
+		colorMode(RGB);
 		background(0);
+		board = new LifeBoard(100, this);
+		board.randomise();
 
-		
-		
 	}
 
-	
-	
 	public void draw()
 	{	
-		
+		background(0);
+		board.render();
+		board.applyRules();
+
 	}
 }
